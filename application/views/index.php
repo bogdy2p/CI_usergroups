e2ab546f94530ff097df9a77626c306230a7fa5e
Popa Bogdan Constantin <bogdan.popa@reea.net>
2014-11-03 10:49:20 +0200

    second commit

---
+++ application/views/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome TITLE</title>
+
+	<style type="text/css">	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome asfdsfsgniter!</h1>
+
+	<div id="body">
+	TEMPPPPLATTEEE
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+</div>
+
+</body>
+</html>
