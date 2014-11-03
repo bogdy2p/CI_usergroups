f72abc182e0eab03cccd84f4fa11f5c0f2cdba5d
Popa Bogdan Constantin <bogdan.popa@reea.net>
2014-11-03 16:20:29 +0200

    removed php-iness from the view

--- application/views/changelog.php
+++ application/views/changelog.php
@@ -1,38 +1,26 @@
 <?php
+$changelog = new Changelog_model();
+$changelog->validation_and_insertion_of_a_new_changelog();
+?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-$test = new Changelog_model();
-$test->validation_and_insertion_of_a_new_changelog();
-echo '
-	<div class ="container">
-		<div class="row">
-			<div class="col-xs-12 col-md-4"> ';
-				$test->generate_changelog_add_new_form();
-   echo '
-			</div>
-			<div class="col-xs-12 col-md-4 "> 	
-				<h2>Latest Applied Changes:</h2>
-			</div>
-			<div class="col-xs-12 col-md-4">
-	<!-- ****************************************************************************************************************   -->
-  ';
-			$test->generate_select_day_form();
- echo '
-	<!-- *****************************************************************************************************************   -->	
-			</div>
-		</div>
-		<div class ="row"> ';
-				 $test->generate_changelog_table_by_post(); 
- echo '   
-		</div>
-		  		
-		<div class="row">';
-		  		 //Crud::print_color_meanings();
-  echo'
-		</div>
-    </div>
-           ';
+<div class ="container">
+  
+      <div class="row">
+            <div class="col-xs-12 col-md-4">
+                        <?php	$changelog->generate_changelog_add_new_form(); ?>
+            </div>
+            <div class="col-xs-12 col-md-4 "> 	
+                      <h2>Latest Applied Changes:</h2>
+            </div>
+            <div class="col-xs-12 col-md-4">
+                        <?php	$changelog->generate_select_day_form(); ?>
+            </div>
+      </div>
+      <div class ="row">
+                    <?php $changelog->generate_changelog_table_by_post();  ?>
+      </div>
+      <div class="row"></div>
+    
+</div>
+          
+<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
