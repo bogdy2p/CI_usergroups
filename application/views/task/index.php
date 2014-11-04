a2f7f876bf82a30d8584d0a79fe4de4fa70121f0
Popa Bogdan Constantin <bogdan.popa@reea.net>
2014-11-04 16:29:17 +0200

    added 'add new task' functionality

--- application/views/task/index.php
+++ application/views/task/index.php
@@ -1,5 +1,11 @@
 <div class="container">
-  
+  <div class="col-xs-12 col-md-4"></div>  
+  <div class="col-xs-12 col-md-4">
+    
+    <a href="task/add">ADD NEW TASK</a>
+    
+  </div>
+  <div class="col-xs-12 col-md-4"></div>
 <?php $task = new Task_model();
       $task->validate_insert_new_task();
 ?>
