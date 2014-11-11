629a616bdea16f369872d273e2aedacc13d0b3a1
Popa Bogdan Constantin <bogdan.popa@reea.net>
2014-11-11 16:32:01 +0200

    Modified User Table.

--- application/views/user/table.php
+++ application/views/user/table.php
@@ -18,30 +18,23 @@
       'cell_alt_end' => '</td>',
       'table_close' => '</table>',
     );
-    $table_data = array(
-      array('User ID',
-        'Username',
-        //'Groups Of Belonging',
-        'View',
-        'Edit',
-        'Delete',
-      ),
-    );
+    $this->table->set_heading('User Id', 'Username','Full Name', 'View','Edit','Delete');
+    
+    
     $user_id_array = $this->user_model->grab_all_user_ids();
     foreach ($user_id_array as $user_id) {
       $temp = $this->user_model->get_user_object($user_id);
-      $groups_array = $this->user_model->get_number_of_groups_for_a_user($user_id);
-      $table_data[] = array(
-        $temp->id,
-        $temp->username,
-        // implode(" / ", $groups_array),
-        '<a href="' . base_url() . 'user/view_user?id=' . $user_id . '"><span class="glyphicon glyphicon-eye-open"></span>',
-        '<a href="' . base_url() . 'user/edit?id=' . $user_id . '"><span class="glyphicon glyphicon-edit spangre"></span>',
-        '<a><span class="glyphicon glyphicon-remove spanred pointer" onclick=confirm_delete_user(' . $user_id . ');></span>',
-      );
+      $this->table->add_row(
+          array('data'=>$temp->id,'class' => 'highlight col-xs-1 col-md-1'),
+          array('data'=>$temp->username,'class'=>'highlight col-xs-4 col-md-3 wordwrap1'),
+          array('data'=>$temp->first_name.' '.$temp->last_name,'class'=>'highlight col-xs-4 col-md-3 wordwrap1'),
+          array('data' => '<a href="' . base_url() . 'user/view_user?id=' . $user_id . '"><span class="glyphicon glyphicon-eye-open"></span>', 'class' => 'highlight col-xs-1 col-md-1'),
+          array('data' => '<a href="' . base_url() . 'user/edit?id=' . $user_id . '"><span class="glyphicon glyphicon-edit spangre"></span>', 'class' => 'highlight col-xs-1 col-md-1', 'colspan' => 1),
+          array('data' => '<a><span class="glyphicon glyphicon-remove spanred pointer" onclick=confirm_delete_user(' . $user_id . ');></span>','class'=> 'highlight col-xs-1 col-md-1')
+        );
     }
     $this->table->set_template($table_template);
-    echo $this->table->generate($table_data);
+    echo $this->table->generate();
     ?>
   </div>
 </div>
